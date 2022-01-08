bea5af13605802aa62722533642e5798cab9e883
Andres-Debugger <76136156+Andres-Debugger@users.noreply.github.com>
2022-01-08 00:09:49 -0400

    Adding Electron, Howler and restructuring the proyect

---
+++ src/Timer.js
@@ -0,0 +1,95 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from "./PlayButton";
+import PauseButton from "./PauseButton";
+import SettingsButton from "./SettingsButton";
+import {useContext, useState, useEffect, useRef} from "react";
+import SettingsContext from "./SettingsContext";
+import {Howl} from 'howler';
+import sound from './alarm.mp3'
+
+const green = '#86AC41';
+const blue = '#4CB5F5';
+
+function Timer() {
+  const settingsInfo = useContext(SettingsContext);
+
+  const [isPaused, setIsPaused] = useState(true);
+  const [mode, setMode] = useState('work'); // work/break/null
+  const [secondsLeft, setSecondsLeft] = useState(0);
+
+  const secondsLeftRef = useRef(secondsLeft);
+  const isPausedRef = useRef(isPaused);
+  const modeRef = useRef(mode);
+
+  function tick() {
+    secondsLeftRef.current--;
+    setSecondsLeft(secondsLeftRef.current);
+  }
+
+  useEffect(() => {
+    function switchMode() {
+      const nextMode = modeRef.current === 'work' ? 'break' : 'work';
+      const nextSeconds = (nextMode === 'work' ? settingsInfo.workMinutes : settingsInfo.breakMinutes) * 60;
+
+      setMode(nextMode);
+      modeRef.current = nextMode;
+
+      setSecondsLeft(nextSeconds);
+      secondsLeftRef.current = nextSeconds;
+      const newsound = new Howl({
+        src: [sound]
+      });
+      
+      // Play the sound.
+      newsound.play();
+    }
+
+    secondsLeftRef.current = settingsInfo.workMinutes * 60;
+    setSecondsLeft(secondsLeftRef.current);
+
+    const interval = setInterval(() => {
+      if (isPausedRef.current) {
+        return;
+      }
+      if (secondsLeftRef.current === 0) {
+        return switchMode();
+      }
+      tick();
+    },1000);
+    
+    return () => clearInterval(interval);
+  }, [settingsInfo]);
+
+  const totalSeconds = mode === 'work'
+    ? settingsInfo.workMinutes * 60
+    : settingsInfo.breakMinutes * 60;
+  const percentage = Math.round(secondsLeft / totalSeconds * 100);
+
+  const minutes = Math.floor(secondsLeft / 60);
+  let seconds = secondsLeft % 60;
+  if(seconds < 10) seconds = '0'+seconds;
+
+  return (
+    <div>
+      <CircularProgressbar
+        value={percentage}
+        text={minutes + ':' + seconds}
+        styles={buildStyles({
+        textColor:'#fff',
+        pathColor:mode === 'work' ? green : blue,
+        tailColor:'rgba(255,255,255,.2)',
+      })} />
+      <div style={{marginTop:'20px'}}>
+        {isPaused
+          ? <PlayButton onClick={() => { setIsPaused(false); isPausedRef.current = false; }} />
+          : <PauseButton onClick={() => { setIsPaused(true); isPausedRef.current = true; }} />}
+      </div>
+      <div style={{marginTop:'20px'}}>
+        <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
